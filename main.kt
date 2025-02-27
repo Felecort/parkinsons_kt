f8abebcea5da5a40a2c6cf0d9a65747df07d0f77
HQ-K4J5YWYV2P <hq-k4j5ywyv2p@HQ-K4J5YWYV2P.local>
2025-02-27 16:11:29 +0300

    init

--- main.kt
+++ main.kt
@@ -6,14 +6,14 @@ import smile.io.Read
 import smile.validation.CrossValidation
 import org.apache.commons.csv.CSVFormat
 
-
 fun main() {
-    val dataset = Read.csv("/Users/hq-k4j5ywyv2p/Documents/dz_kotlin/parkinsons_updrs.data", CSVFormat.DEFAULT.withFirstRecordAsHeader())
-    val formula = Formula.lhs("status")
-    
+    val dataset = Read.csv("src/main/resources/parkinsons_updrs.csv", CSVFormat.DEFAULT.withFirstRecordAsHeader())
+    val formula = Formula.lhs("total_UPDRS")
+
     val res = CrossValidation.regression(
         6, formula, dataset,
-        { f, data -> RandomForest.fit(f, data) })
+        { f, data -> RandomForest.fit(f, data) }
+    )
 
     println(res)
 }
